9607c295ff4ec8891255d5df6414e53b5011b57b
Hongbo Zhang <bobzhang1988@gmail.com>
2018-03-06 21:16:51 +0800

    tweak config

--- examples/set.ml
+++ examples/set.ml
@@ -1,13 +1,16 @@
 
 module S = Belt.Set.Int 
+let data = [|1;2;3;4;4;3|]
 
-let v  = S.ofArray [|1;2;3;4;4;3|]
+let v  = S.ofArray data 
 
 let () = Js.log (S.toArray v)
 
 module S0 = Belt.Set
-module Id = (val Belt.Id.comparableU ~cmp:(fun[@bs] (a : int) b -> b - a))
+module Id = 
+  (val Belt.Id.comparableU 
+    ~cmp:(fun[@bs] (a : int) b -> b - a))
 
-let v1 = S0.ofArray ~id:(module Id) [|1;2;3;4;4;3|]
+let v1 = S0.ofArray ~id:(module Id) data
 
 let () = Js.log (S0.toArray v1)
